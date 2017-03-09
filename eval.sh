e22369461d3f036476f78f49c555f6bc944d3ae4
Prakhar <prakhar.kumar91@gmail.com>
2017-03-09 22:37:19 +0530

    updated script

--- eval.sh
+++ eval.sh
@@ -2,6 +2,6 @@
 for i in {1..10};
 do
 	echo "Test case $i";
-	./ac_circuit_solver ./examples/Testcases/Testcase$i/top.cir $i.svg
+	./ac_circuit_solver ./examples/Testcases/Testcase$i/top.cir $i.svg results$i.txt 
 	echo "---------------";
 done;
